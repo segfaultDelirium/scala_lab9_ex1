3af105242750df944ea3cccc4a99bbfa93796c67
segfaultdelirium <krissthompson@protonmail.com>
2023-12-20 08:44:15 +0100

    add files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,6 @@ def distanceSum(
     distanceFunction: (Int, Int) => Double
 ): Double = {
   zipped
-    // .map((a, b) => math.pow((a - b).toDouble, 2.0))
     .map((a, b) => distanceFunction(a, b))
     .reduce((acc, x) => acc + x)
 }
@@ -25,41 +24,27 @@ def distFirstPart(
 def dist(
     collection1: Iterable[Int],
     collection2: Iterable[Int],
-    f: Int => Int = identity
+    functionToApplyToEach: Int => Int = identity
 ): Double = {
 
   distanceSum(
-    distFirstPart(collection1, collection2, f),
+    distFirstPart(collection1, collection2, functionToApplyToEach),
     (a: Int, b: Int) => math.pow((a - b).toDouble, 2.0)
   )
-  // .map((a, b) => math.pow((a - b).toDouble, 2.0))
-  // distanceSum(
-  //   collection1
-  //     .zip(collection2)
-  //     .map((a, b) => (f(a), f(b))),
-  //   (a: Int, b: Int) => math.pow((a - b).toDouble, 2.0)
-  // )
-  // .map((a, b) => math.pow((a - b).toDouble, 2.0))
-  // .reduce((acc, x) => acc + x)
 }
 
 def dist2(
     collection1: Iterable[Int],
     collection2: Iterable[Int],
-    f: (Int, Int) => Double
+    distanceFunction: (Int, Int) => Double
 ): Double = {
-
-  distanceSum(distFirstPart(collection1, collection2), f)
-  // (a: Int, b: Int) => math.pow((a - b).toDouble, 2.0)
-
-  // 0.0
+  distanceSum(distFirstPart(collection1, collection2), distanceFunction)
 }
 
 @main def hello: Unit = {
   println("Hello world!")
   println(dist(List(1, 3), List(1, 3)))
   println(dist(List(1, 1), List(1, 3)))
-  // println(dist(List(1, 1), List(1, 3), identity))
   println(dist(List(1, 4), List(1, -4), math.abs))
   println(
     dist2(List(1, 4), List(1, -4), (x: Int, y: Int) => { math.abs(x - y) })
